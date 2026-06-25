aa209a5fcf2f6b6e43a5c84e1d352ebc522fcfbb
xidstar <xidstar@gmail.com>
2026-06-24 21:37:16 -0400

    resume update

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -11,7 +11,7 @@ import plane from './plane.png'
 import wtrust from './wtrust.png'
 import lenels2 from './lenels2.png'
 
-import resume from "./Sidney-Oluoch-resume.pdf"
+import resume from "./Sidney_Oluoch-Resume.pdf"
 
 
 export { logo, portfolio, barbershop, dashboard, tshirt, box, pin, file, contactText, resume, plane, wtrust, lenels2};
